7018db75b8e4825a92e2a4bd5c721acfa962b7aa
Ravindra Palli <ravindra.palli@cgi.com>
2020-06-12 11:53:54 +0530

    Fixed auto changelog issues with updating log

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -4,14 +4,14 @@ CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]')"
 DEVELOPMENT_BRANCH="development"
 STAGING_BRANCH="staging"
 
-AUTO_CHANGE_LOG_COMMAND="npx auto-changelog -p --config .auto-changelog.json"
+AUTO_CHANGE_LOG_COMMAND="npx auto-changelog --stdout --config .auto-changelog.json"
 
 if [ $CURRENT_BRANCH = $DEVELOPMENT_BRANCH ]; then
-    AUTO_CHANGE_LOG_COMMAND+=" --tag-pattern '.+-dev'"
+    AUTO_CHANGE_LOG_COMMAND+=" --tag-pattern .+-dev"
 elif [ $CURRENT_BRANCH = $STAGING_BRANCH ]; then
-    AUTO_CHANGE_LOG_COMMAND+=" --tag-pattern '^\d.+-rc.\d$'"
+    AUTO_CHANGE_LOG_COMMAND+=" --tag-pattern ^\d.+-rc.\d$"
 else
-    AUTO_CHANGE_LOG_COMMAND+=" --tag-pattern '^\d+.\d+.\d+$'"
+    AUTO_CHANGE_LOG_COMMAND+=" --tag-pattern ^\d+.\d+.\d+$"
 fi;
 
 $AUTO_CHANGE_LOG_COMMAND
